c562b2057838ebcbc60ce37cd325b009bd4c882a
xtansy <xtansy@mail.ru>
2024-04-24 20:47:18 +0300

    add define vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 const { resolve } = path;
 
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"pages": resolve(__dirname, "./src/pages"),
-			"widgets": resolve(__dirname, "./src/widgets"),
-			"shared": resolve(__dirname, "./src/shared"),
-			"entities": resolve(__dirname, "./src/entities"),
-			"features": resolve(__dirname, "./src/features"),
-		}
-	}
-})
+    define: {
+        __APP_ENV__: process.env.VITE_URL,
+    },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            pages: resolve(__dirname, "./src/pages"),
+            widgets: resolve(__dirname, "./src/widgets"),
+            shared: resolve(__dirname, "./src/shared"),
+            entities: resolve(__dirname, "./src/entities"),
+            features: resolve(__dirname, "./src/features"),
+        },
+    },
+});
