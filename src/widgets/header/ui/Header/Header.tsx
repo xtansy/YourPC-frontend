82d84def4d2b20cbb72a9b5150f53f8d98d52a22
xtansy <xtansy@mail.ru>
2024-04-24 20:09:43 +0300

    fix for deploy

--- src/widgets/header/ui/Header/Header.tsx
+++ src/widgets/header/ui/Header/Header.tsx
@@ -1,21 +1,19 @@
-import css from "./header.module.css";
-
 import { type ReactNode } from "react";
 
 import { Logo } from "shared/ui";
 
 interface HeaderProps {
-	rightSlot: ReactNode;
+    rightSlot: ReactNode;
 }
 
 export const Header: React.FC<HeaderProps> = ({ rightSlot }) => {
-	return (
-		<div className={css.header}>
-			<Logo />
-			<div className={css.right}>
-				{rightSlot}
-				{/* theme switcher */}
-			</div>
-		</div>
-	);
+    return (
+        <div>
+            <Logo />
+            <div>
+                {rightSlot}
+                {/* theme switcher */}
+            </div>
+        </div>
+    );
 };
